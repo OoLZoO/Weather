3fa5e65a7305de237ae9635f450521078e101f06
ape06 <zhenhao.li@kellogg.ox.ac.uk>
2023-04-26 17:20:27 +0100

    add 2nd test and impl

--- src/main/java/com/teamoptimization/CachingForecaster.java
+++ src/main/java/com/teamoptimization/CachingForecaster.java
@@ -2,10 +2,16 @@ package com.teamoptimization;
 
 import java.io.IOException;
 import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CachingForecaster implements ForecasterClient {
 
     private ForecasterClient delegate;
+
+    private List<String> history = new ArrayList<>();
+
+    private List<MetOfficeForecasterClient.Forecast> cachedForecasts = new ArrayList<>();
 
 
     public CachingForecaster(ForecasterClient delegate) {
@@ -14,6 +20,12 @@ public class CachingForecaster implements ForecasterClient {
 
     @Override
     public MetOfficeForecasterClient.Forecast forecast(DayOfWeek dayOfWeek, String place) throws IOException {
-        return delegate.forecast(dayOfWeek, place);
+        String entry = dayOfWeek.toString() + place;
+        if(!history.contains(entry)) {
+            history.add(entry);
+            cachedForecasts.add(delegate.forecast(dayOfWeek, place));
+            return cachedForecasts.get(cachedForecasts.size() - 1);
+        }
+        return cachedForecasts.get(history.indexOf(entry));
     }
 }
--- src/test/java/com/develogical/CachingForecasterTest.java
+++ src/test/java/com/develogical/CachingForecasterTest.java
@@ -11,10 +11,10 @@ import java.time.DayOfWeek;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class CachingForecasterTest {
+
     @Test
     public void getsForecastFromDelegateIfNeverSeenBefore() throws IOException {
         ForecasterClient delegate = mock(ForecasterClient.class);
@@ -27,5 +27,20 @@ public class CachingForecasterTest {
 
         assertThat(actual, equalTo(forecast));
         verify(delegate).forecast(DayOfWeek.WEDNESDAY, "Oxford");
+    }
+
+    @Test
+    public void getsForecastFromDelegateWhenSeenBefore() throws IOException {
+        ForecasterClient delegate = mock(ForecasterClient.class);
+        MetOfficeForecasterClient.Forecast forecast = new MetOfficeForecasterClient.Forecast(0, 16, "Generally clear but cold.");
+        given(delegate.forecast(DayOfWeek.WEDNESDAY, "Oxford")).willReturn(forecast);
+
+        CachingForecaster cachingForecaster = new CachingForecaster(delegate);
+
+        cachingForecaster.forecast(DayOfWeek.WEDNESDAY, "Oxford");
+        MetOfficeForecasterClient.Forecast actual = cachingForecaster.forecast(DayOfWeek.WEDNESDAY, "Oxford");
+
+        verify(delegate, times(1)).forecast(DayOfWeek.WEDNESDAY, "Oxford");
+        assertThat(actual, equalTo(forecast));
     }
 }
